683697c9dfee534f0db859a3104dd15fe58c0149
AntonShpak <Onlytimehasthesense@gmail.com>
2019-12-03 20:42:24 +0300

    Добавьте файлы проекта.

---
+++ Mordor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mordor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
