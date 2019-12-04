b3fa9c508a4fd5223a8bf301f11075cd1eb96649
AntonShpak <Onlytimehasthesense@gmail.com>
2019-12-04 03:16:49 +0300

    Adding Registration and Db

---
+++ Mordor/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mordor.Models;
+
+namespace Mordor.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AppUserContext db;
+        public AccountController(AppUserContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await db.AppUsers.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(model.Email); 
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await db.AppUsers.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    db.AppUsers.Add(new AppUser { Email = model.Email, Password = model.Password });
+                    await db.SaveChangesAsync();
+                    await Authenticate(model.Email);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Mordor/Models/AppUserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mordor.Models
+{
+    public class AppUserContext : DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public AppUserContext(DbContextOptions<AppUserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
